d8f6fa0253975917f4011c51ed46d891a6957959
Robin Richtsfeld <robin.richtsfeld@gmail.com>
2018-04-23 19:31:28 +0200

    Add XObject::forEach method

--- src/de/androbin/json/XObject.java
+++ src/de/androbin/json/XObject.java
@@ -1,5 +1,6 @@
 package de.androbin.json;
 
+import java.util.function.*;
 import org.json.simple.*;
 
 public final class XObject {
@@ -11,6 +12,11 @@ public final class XObject {
   
   public XObject( final JSONObject obj ) {
     this.obj = obj;
+  }
+  
+  @ SuppressWarnings( "unchecked" )
+  public void forEach( final BiConsumer<String, XValue> action ) {
+    obj.forEach( ( key, value ) -> action.accept( (String) key, new XValue( value ) ) );
   }
   
   public XValue get( final String key ) {
